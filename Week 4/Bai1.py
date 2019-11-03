3da948c09fc74912aa86829f543621910b08d906
quanghung22092k <55677290+quanghung22092k@users.noreply.github.com>
2019-11-03 23:19:40 +0700

    Update Bai1

--- Week 4/Bai1.py
+++ Week 4/Bai1.py
@@ -1,7 +1,11 @@
-s = input()
-x = ""
-while s != "":
-	i = (len(s) - 1) // 2
-	x += s[i]
-	s = s[:i] + s[i+1:]
-print(x)
+# Ban đầu tưởng hiêu cầu của đề là mã hóa
+# s = input()
+# x = ""
+# while s != "":
+# 	i = (len(s) - 1) // 2
+# 	x += s[i]
+# 	s = s[:i] + s[i+1:]
+# print(x)
+
+arr = input()
+print(arr[-2::-2]+arr[::2])
